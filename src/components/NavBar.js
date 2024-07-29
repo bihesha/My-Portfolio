2763d12e7fac9128a2a7142f10bfde26ce530c5c
Bihesha Weththasingha <bihesha@IRCSL.LOCAL>
2024-07-29 14:12:41 +0530

    Update menu icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -53,7 +53,7 @@ const NavBar = () => {
     }
     
     return (
-        <header className='relative flex items-center justify-between w-full px-32 py-8 font-medium dark:text-light'>
+        <header className='relative flex items-center justify-between w-full px-32 py-8 font-medium sm:px-12 dark:text-light'>
 
             <button className='flex-col items-center justify-center hidden lg:flex' onClick={handleClick}>
                 <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
